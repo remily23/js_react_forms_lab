176b38f075fbca9187da0c5b9ec74a311aacfd18
Hezekiah Adeusi <hezekiaha58@gmail.com>
2023-11-30 17:02:28 +0000

    - Refactored Cake.js to dynamically render the properties of each cake object - Rendured each ingredient from the ingredients Array using the key property

--- src/components/Cake.js
+++ src/components/Cake.js
@@ -1,9 +1,28 @@
-const Cake = () => {
+// Deconstructed cake Object in cakes Array
+const Cake = ({ cake }) => {
 
   return (
-    <>
-        <h2>This is a cake</h2>
-    </>
+    <div className="cake-container">
+
+      {/* inserts the cakeName for each cake Object */}
+      <h2>{cake.cakeName}</h2>
+
+      <ul>
+        {/* - Map through the ingredients Array
+            - Display every ingredient Object as an <li> 
+              using the key property as a unique ID
+            */}
+        {cake.ingredients.map((ingredient) => (
+          <li key={ingredient}>
+            {ingredient}
+          </li>
+        ))}
+      </ul>
+
+      <p>Price: ${cake.price}</p>
+
+      <p>Rating: {cake.rating}</p>
+    </div>
   )
 }
 
